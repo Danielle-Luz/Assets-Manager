366a80760b63abadf21a03e1f2f7ec2c20d85532
Danielle Luz <daniln2209@gmail.com>
2026-06-06 17:25:00 -0300

    feat: user entity

---
+++ src/main/java/io/github/danielleluz/assetsmanager/entities/UserEntity.java
@@ -0,0 +1,30 @@
+package io.github.danielleluz.assetsmanager.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.validation.constraints.*;
+
+import java.util.UUID;
+
+@Entity
+public class UserEntity {
+    @GeneratedValue
+    private UUID id;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Min(8)
+    @Pattern(regexp = "^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[^0-9a-zA-Z]+)(?=.*[0-9]+).+$") // contains one or more lower case characters, one or more upper case characters, one or more symbols and one or more numbers
+    private String password;
+
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z]+$") // contains only one or more letters
+    private String firstName;
+
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z]+$")
+    private String lastName;
+}
