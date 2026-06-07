e34eb465dd4f35747af2784bdb9745f552d68386
Danielle Luz <daniln2209@gmail.com>
2026-06-07 18:59:50 -0300

    feat: add id field in step action criteria

--- src/main/java/io/github/danielleluz/assetsmanager/entities/StepActionCriteriaEntity.java
+++ src/main/java/io/github/danielleluz/assetsmanager/entities/StepActionCriteriaEntity.java
@@ -5,16 +5,24 @@ import io.github.danielleluz.assetsmanager.enums.ValueTypeEnum;
 import io.github.danielleluz.assetsmanager.validation.annotations.IsValidClassField;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class StepActionCriteriaEntity {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
     @NotBlank
     @IsValidClassField(className = AgreementEntity.class)
     private String field;
