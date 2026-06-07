2cdd7fb53d42a229f6a990421956136fdd3c0d7a
Danielle Luz <daniln2209@gmail.com>
2026-06-07 18:01:11 -0300

    refactor: add enumareted annotation on table name enum

--- src/main/java/io/github/danielleluz/assetsmanager/entities/PermissionEntity.java
+++ src/main/java/io/github/danielleluz/assetsmanager/entities/PermissionEntity.java
@@ -1,9 +1,7 @@
 package io.github.danielleluz.assetsmanager.entities;
 
 import io.github.danielleluz.assetsmanager.enums.TableNameEnum;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -16,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PermissionEntity {
     @NotBlank
+    @Enumerated(EnumType.STRING)
     private TableNameEnum tableName;
 
     @NotNull
